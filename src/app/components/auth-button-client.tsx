6cf51e88371e893402c2c8377d84a63ba846e511
Jorge Mejia <jorgemejia2304@gmail.com>
2023-11-07 20:19:46 -0500

    fix route

--- src/app/components/auth-button-client.tsx
+++ src/app/components/auth-button-client.tsx
@@ -4,25 +4,15 @@ import { type Session, createClientComponentClient } from '@supabase/auth-helper
 import { GitHubIcon } from './icons'
 import { useRouter } from 'next/navigation'
 import { Button } from '@nextui-org/react'
-
 export function AuthButton ({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient()
-  const getURL = () => {
-    let url =
-      'https://clone-x.pages.dev' ?? // Set this to your site URL in production env.
-      'http://localhost:3000'
-    // Make sure to include `https://` when not localhost.
-    url = url.includes('http') ? url : `https://${url}`
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-    return url
-  }
   const router = useRouter()
+
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: `${getURL()}/auth/callback`
+        redirectTo: 'https://clone-x.pages.dev/auth/callback'
       }
     })
   }
