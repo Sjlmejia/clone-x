bc0094f79010af3931ca37af8c8f38f00e9f10ed
Jorge Mejia <jorgemejia2304@gmail.com>
2023-11-07 20:14:39 -0500

    fix test

--- src/app/components/auth-button-client.tsx
+++ src/app/components/auth-button-client.tsx
@@ -9,7 +9,7 @@ export function AuthButton ({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient()
   const getURL = () => {
     let url =
-      process?.env?.NEXT_PUBLIC_ROUTE_DIRECTION ?? // Set this to your site URL in production env.
+      'https://clone-x.pages.dev' ?? // Set this to your site URL in production env.
       'http://localhost:3000'
     // Make sure to include `https://` when not localhost.
     url = url.includes('http') ? url : `https://${url}`
