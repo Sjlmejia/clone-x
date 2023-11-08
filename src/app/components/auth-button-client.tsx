c886fc862885966d66bc86f6857b328169a6a6e5
Jorge Mejia <jorgemejia2304@gmail.com>
2023-11-07 20:25:26 -0500

    fix route

--- src/app/components/auth-button-client.tsx
+++ src/app/components/auth-button-client.tsx
@@ -12,7 +12,7 @@ export function AuthButton ({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: 'https://clone-x.pages.dev/auth/callback'
+        redirectTo: 'http://localhost:3000/auth/callback'
       }
     })
   }
