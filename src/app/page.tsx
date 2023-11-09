5a1958fc1876c31d6d6fea0c6f63ead598c74af0
Jorge Mejia <jorgemejia2304@gmail.com>
2023-11-08 21:20:22 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { AuthButtonServer } from './components/auth-button-server'
 import { redirect } from 'next/navigation'
-import PostLists from './components/posts-lists'
-import { type Post } from './types/posts'
-import { ComposePost } from './components/compose-post'
 import supabaseServer from './supabaseServer'
 export const runtime = 'edge'
 export default async function Home () {
@@ -17,8 +14,11 @@ export default async function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className='max-w-[800px] w-full mx-auto border-l border-r border-white/80 min-h-screen'>
-        <ComposePost userAvatarUrl={session?.user?.user_metadata?.avatar_url}/>
-        <PostLists posts={posts as Post[]}/>
+        <pre>
+          {
+            JSON.stringify(posts, null, 2)
+          }
+        </pre>
       </section>
       <AuthButtonServer/>
     </main>
