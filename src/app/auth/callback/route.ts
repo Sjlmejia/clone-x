f19ed0283e959ceba44a4c8fda0d12601889aa7b
Jorge Mejia <jorgemejia2304@gmail.com>
2023-11-11 16:24:00 -0500

    ADD protection

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,6 @@
-import supabaseRoute from '@/app/supabaseRoute'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse, type NextRequest } from 'next/server'
-
+import { cookies } from 'next/headers'
 // esto es una opcion de Next.js para evitar que cachee de froma
 // estatica la ruta, y de que siempre se ejecute en le servidor
 export const dynamic = 'force-dynamic'
@@ -9,7 +9,8 @@ export async function GET (request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
   if (code !== null) {
-    await supabaseRoute().auth.exchangeCodeForSession(code)
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
   }
   return NextResponse.redirect(requestUrl.origin)
 }
